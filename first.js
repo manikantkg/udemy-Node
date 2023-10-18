dec81473c6be0df88bdadcc01f64d2af710e4649
manikantkg <manikantacoder007@gmail.com>
2023-10-18 13:04:36 +0530

    variables & data types

--- first.js
+++ first.js
@@ -1,1 +1,23 @@
-console.log("Hai")
+// console.log("Hai")
+
+a = 'abcd'
+b = ['123','234', '345']
+c = ['mani', '33', 'software Engineer']
+d = 23
+
+console.log(a,b, c, d)
+
+console.log("a is :",typeof(a))
+
+console.log("b is :",typeof(b))
+
+console.log("c is :",typeof(c))
+
+console.log("d is :",typeof(d))
+
+b.push('abc'); //adding last element
+
+console.log(b)
+
+b.pop()
+console.log(b) // removing last element
