b6b1c87d8ded9ff36d6bd54f0498e5b29cb05c11
manikantkg <manikantacoder007@gmail.com>
2023-10-29 07:50:55 +0530

    routing added in server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const http = require('http')
+const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Server is running!!!👌👌');
+    // console.log(req.url);
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is overview page');
+    } else if (pathName === '/product') {
+        res.end('This is a Product page');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
 });
 
+    // res.end('Server is running!!!👌👌');
 server.listen(8080, 'localhost', () => {
     console.log('server working fine')
 });
