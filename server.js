99677ecd21343823c340792c015f4f3eb775f076
manikantkg <manikantacoder007@gmail.com>
2023-11-01 12:10:03 +0530

    templates added

--- server.js
+++ server.js
@@ -5,32 +5,42 @@ const fs = require('fs')
 
 
 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const temProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url);
     const pathName = req.url;
 
+    //Overview Page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is overview page');
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+        res.end(tempOverview);
+
+        //Product Page
     } else if (pathName === '/product') {
         res.end('This is a Product page');
+
+        //API page
     } else if (pathName === '/api') {
 
         // fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
         //     const prodData = JSON.parse(data);
-        res.writeHead(200, {'Content-type': 'application/json'});
+        res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
     } else {
-    res.writeHead(404, {
-        'Content-type': 'text/html',
-        'my-own-header': 'hello-world'
-    });
-    res.end('<h1>Page not found</h1>');
-}
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
 });
 
 // res.end('Server is running!!!👌👌');
